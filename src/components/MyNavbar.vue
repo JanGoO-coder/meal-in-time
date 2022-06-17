9e5ffcef45a8eca9b9eae6ff35967e729c06aecc
JanGoO-coder <asadalijangoo@gmail.com>
2022-06-17 20:01:03 +0500

    Navigation added

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -7,7 +7,7 @@ const userStore = useAdminStore()
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-gradient-to-b from-orange-300 to-orange-400 py-3 !text-lg !font-bold shadow-2xl">
         <div class="container">
-            <a class="navbar-brand me-4" href="#">
+            <a class="navbar-brand me-5" href="#">
                 <img class="h-12 rounded-full" src="../assets/meal-in-time-logo.png" alt="Meal In Time Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -23,6 +23,17 @@ const userStore = useAdminStore()
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
+                            Orders
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><router-link to="/" class="dropdown-item">Pending Orders</router-link></li>
+                            <li><router-link to="/" class="dropdown-item">Recived Orders</router-link></li>
+                            <li><router-link to="/" class="dropdown-item">Completed Orders</router-link></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             Tables
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -30,10 +41,18 @@ const userStore = useAdminStore()
                             <li><router-link to="/add-table" class="dropdown-item">Add Table</router-link></li>
                             <li><router-link to="/edit-table" class="dropdown-item">Edit Table</router-link></li>
                             <li><router-link to="/remove-table" class="dropdown-item">Remove Table</router-link></li>
-                            <!-- <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Menu
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><router-link to="/" class="dropdown-item">View Menu</router-link></li>
+                            <li><router-link to="/" class="dropdown-item">Add Menu Item</router-link></li>
+                            <li><router-link to="/" class="dropdown-item">Edit Menu Item</router-link></li>
+                            <li><router-link to="/" class="dropdown-item">Remove Menu Item</router-link></li>
                         </ul>
                     </li>
                 </ul>
